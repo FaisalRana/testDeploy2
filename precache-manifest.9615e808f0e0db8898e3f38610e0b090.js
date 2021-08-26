55ba8809c1d56a00d6845b79709c43067c4db837
Faisal Rana <inquisitive@gmail.com>
2021-08-25 20:57:19 -0700

    Updates

---
+++ precache-manifest.9615e808f0e0db8898e3f38610e0b090.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a17f92bfdbe60dcd9c8422f94fb41e",
+    "url": "/testDeploy2/index.html"
+  },
+  {
+    "revision": "4ae4c2e8482061694591",
+    "url": "/testDeploy2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b3d1941d281879b554a6",
+    "url": "/testDeploy2/static/js/2.32866a27.chunk.js"
+  },
+  {
+    "revision": "4ae4c2e8482061694591",
+    "url": "/testDeploy2/static/js/main.d5e3bfcc.chunk.js"
+  },
+  {
+    "revision": "36fc08824c00114e19a3",
+    "url": "/testDeploy2/static/js/runtime-main.4606137e.js"
+  }
+]);
